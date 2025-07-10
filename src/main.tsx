d09f1b9b1f3a5c551f6588d30c59c96aeb4deb83
Ekerbam8 <Dave@workfor7.com>
2025-07-10 12:59:43 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import CustomizerDashboard from "./CustomizerDashboard";
-import "./index.css"; // TailwindCSS import
+import "./index.css"; // Make sure this includes Tailwind styles
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>

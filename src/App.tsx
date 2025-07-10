36f149115e6958317cdd7d3b8a192d7673d669a6
Ekerbam8 <Dave@workfor7.com>
2025-07-10 13:08:38 -0400

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CustomizerDashboard from "./CustomizerDashboard";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-white text-gray-900">
+      <CustomizerDashboard />
+    </div>
+  );
+}
+
+export default App;
